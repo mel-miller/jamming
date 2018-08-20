644f1ae3f1348c5840448238d6d5b6d1ec2f67bb
M Miller <miller.2676@osu.edu>
2018-08-19 17:05:53 -0700

    set default state of playlist as empty string

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
 
     this.state = {
       searchResults: [],
-      playlistName: 'New Playlist',
+      playlistName: '',
       playlistTracks: []
     };
 
