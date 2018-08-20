8e49876c5dd6cf4546829942b8068e431ed359c9
M Miller <miller.2676@osu.edu>
2018-08-19 18:42:49 -0700

    fix playlist to show playlistName value instead of default Value

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -17,7 +17,7 @@ class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
-        <input onChange={this.handleNameChange} defaultValue={this.props.playlistName}/>
+        <input onChange={this.handleNameChange} value={this.props.playlistName}/>
           <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
         <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
       </div>
