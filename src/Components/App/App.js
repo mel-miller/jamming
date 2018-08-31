886ef861aa480511a84576f5c463feaff18cad47
M Miller <miller.2676@osu.edu>
2018-08-30 17:51:59 -0700

    reformat long components in App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -60,8 +60,18 @@ class App extends React.Component {
       <div className="App">
         <SearchBar onSearch={this.search}/>
         <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} onSearch={this.search}/>
-          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist}/>
+          <SearchResults
+            searchResults={this.state.searchResults}
+            onAdd={this.addTrack}
+            onSearch={this.search}
+          />
+          <Playlist
+            playlistName={this.state.playlistName}
+            playlistTracks={this.state.playlistTracks}
+            onRemove={this.removeTrack}
+            onNameChange={this.updatePlaylistName}
+            onSave={this.savePlaylist}
+          />
         </div>
       </div>
     </div>);
